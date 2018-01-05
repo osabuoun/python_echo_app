34bfca1a4517302ca11d3e1767010d1a24c21d07
Osama <oabuoun1>
2018-01-05 10:33:41 +0000

    adding timers

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-import sys
+import sys, time
+from random import randint
 
 params = sys.argv[1]
 with open("./log", "a") as myfile:
 	myfile.write(params + "\n")
-print(params)
+	print(params)
+	d = randint(10, 60)
+	myfile.write("------- Waiting for " + str(d) + "------" + "\n")
+	print("------- Waiting for " + str(d) + "------")
+	time.sleep(d)
+	myfile.write("------- Finished ------" + "\n")
+	print("--------- Finished ----------------")
 sys.exit(0)
--- my_script.py
+++ my_script.py
@@ -1,8 +1,9 @@
 # Sample taken from pyStrich GitHub repository
 # https://github.com/mmulqueen/pyStrich
 import time
+from random import randint
 id = 0
 while(1):
 	print(id)
 	id = id+1
-	time.sleep(1)
+	time.sleep(randint(10, 100))
