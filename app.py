3cf75602f74c3342b05bee187ea96a813fa9f351
Osama <oabuoun1>
2018-01-09 20:50:12 +0000

    adding http

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
-import sys, time
+import sys, time, json
 from random import randint
 
 params = sys.argv[1]
+print("-------------------------" +params)
+data_json = json.loads(params)
 with open("./log", "a") as myfile:
-	myfile.write(params + "\n")
-	print(params)
-	d = randint(10, 60)
+	myfile.write(str(data_json) + "\n")
+	print(data_json)
+	#d = randint(10, 60)
+	d = int(data_json['sleep'])
+	server = data_json['server']
 	myfile.write("------- Waiting for " + str(d) + "------" + "\n")
 	print("------- Waiting for " + str(d) + "------")
 	time.sleep(d)
 	myfile.write("------- Finished ------" + "\n")
 	print("--------- Finished ----------------")
+	'''
+	local_url =  server + "/api/v1/query?query=" + query
+	try:
+		return requests.post(local_url).json()
+	except Exception as e:
+		return {"status": "failed", "message":e}
+	'''
 sys.exit(0)
--- test_thread.py
+++ test_thread.py
@@ -1,26 +1,30 @@
 from threading import Thread
 import sys, time, os
 
-def thread1():
+value = set()
+def thread1(value):
 	print("Thread 1 - started")
 	for x in range(1,10):
 		time.sleep(1)
-		print("thread 1 " + str(x))
+		print("thread 1 " + str(x) + " - value : " + str(value))
+		value.add("th1-" + str(x)) 
 	print("Thread 1 - Finished")
 
-def thread2():
+def thread2(value):
 	print("Thread 2 - started")
 	for x in range(1,5):
 		time.sleep(1)
-		print("thread 2 " + str(x))
+		print("thread 2 " + str(x) + " - value : " + str(value))
+		value.add("th2-" + str(x)) 
+
 	print("Thread 1 - Finished")
-	os._exit(0)
+	sys.exit(0)
 	for x in range(1,5):
 		time.sleep(1)
 		print("thread 2 - Phase 2 : " + str(x))
 
 if __name__ == '__main__':
-    thread1_thread = Thread(target = thread1, args = ())
+    thread1_thread = Thread(target = thread1, args = (value,))
     thread1_thread.start()
-    thread2_thread = Thread(target = thread2, args = ())
+    thread2_thread = Thread(target = thread2, args = (value,))
     thread2_thread.start()
